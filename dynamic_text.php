b6ace2fd6ffe2ae5424f785a0efa63105ba23865
LucNieto <noodle3030@hotmail.com>
2019-01-15 22:38:38 -0600

    Dynamic text with embedded PHP

--- dynamic_text.php
+++ dynamic_text.php
@@ -1,3 +1,9 @@
+
+<!DOCTYPE HTML>
+    <meta charset="UTF-8"/>
+    <title>PHP 2017</title>
+</head>
+<body>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,3 +11,9 @@
  * Date: 15/01/2019
  * Time: 21:53
  */
+$title = "Lucas Films";
+?>
+<h1><?php echo $title ?></h1>
+</body>
+
+
